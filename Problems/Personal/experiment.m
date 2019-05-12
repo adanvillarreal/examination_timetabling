573d6c5ed9f9ccc318bdcf5215f0a9e353b02f41
Adan Villarreal <adanvillarreal0@gmail.com>
2019-05-12 23:43:30 +0200

    Removes old files and adds experiment file

--- Problems/Personal/experiment.m
+++ Problems/Personal/experiment.m
@@ -1,9 +1,17 @@
-function [offset, solutions] = experiment()
-    offset = {};
+function [res] = experiment()
+    problem = {};
     solutions = {};
-    for i=1:5
-        offset{end+1} = i * 250;
-        solutions{end+1} = runner(i*250);
+    res = zeros(50, 5);
+    count = 1;
+    for i=1:9
+        for j=0:5
+        res(count, 1) = i * 5 / 100;
+        res(count, 2) = j * 12;
+        [a, b, c] = runner_exm(j*8, 8, "sp" + string(i * 5), 150, 50000);
+        res(count, 3) = b;
+        res(count, 4) = c;
+        count = count + 1;
+        end
     end
 end
 
